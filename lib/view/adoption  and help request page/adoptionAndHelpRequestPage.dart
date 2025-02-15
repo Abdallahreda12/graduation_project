9d02b32a9327d60bd51592fc02c2a8ae0754adf5
Abdallah Reda <abdallahreda664@gmail.com>
2025-02-15 16:24:06 +0200

    optimize code in adoptionAndHelpRequestPage

--- lib/view/adoption  and help request page/adoptionAndHelpRequestPage.dart
+++ lib/view/adoption  and help request page/adoptionAndHelpRequestPage.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:graduation_project/core/Widgets/headerOfAdotinAndHelpPage.dart';
 import 'package:graduation_project/core/util/appImages.dart';
 import 'package:graduation_project/core/util/colors.dart';
-import 'package:graduation_project/core/util/styles.dart';
+import 'package:graduation_project/view/adoption%20%20and%20help%20request%20page/widgets/selectTypeOfRequest.dart';
 
 class AdoptionAndHelpRequestPage extends StatefulWidget {
   const AdoptionAndHelpRequestPage({super.key});
@@ -14,7 +14,7 @@ class AdoptionAndHelpRequestPage extends StatefulWidget {
 
 class _AdoptionAndHelpRequestPageState
     extends State<AdoptionAndHelpRequestPage> {
-  String requestType = "help";
+  String selectedType = "help";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,89 +37,29 @@ class _AdoptionAndHelpRequestPageState
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                GestureDetector(
+                SelectTypeOfRequest(
+                  isSelected: selectedType == "help",
                   onTap: () {
                     setState(() {
-                      requestType = "help";
+                      selectedType = "help";
                     });
                   },
-                  child: Container(
-                    padding: EdgeInsets.all(1),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6),
-                      border: requestType == "help"
-                          ? Border.all(
-                              color: ColorsApp.secondaryColor, width: 1)
-                          : null,
-                    ),
-                    child: Container(
-                      width: (MediaQuery.sizeOf(context).width / 2) - 35,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          fit: BoxFit.fill,
-                          scale: 2,
-                          image: AssetImage(Assets.imagesAnimalPhoto2),
-                        ),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 5, left: 10),
-                            child: Text(
-                              "help",
-                              style: AppStyles.urbanistMedium16(context)
-                                  .copyWith(color: ColorsApp.secondaryColor),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
+                  type: "help",
+                  borderColor: ColorsApp.secondaryColorOpicaty,
+                  image: Assets.imagesAnimalPhoto2,
+                  textColor: ColorsApp.secondaryColor,
                 ),
-                GestureDetector(
-                  onTap: () {
-                    setState(
-                      () {
-                        requestType = "adoption";
-                      },
-                    );
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(1),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6),
-                      border: requestType == "adoption"
-                          ? Border.all(color: ColorsApp.primaryColor, width: 1)
-                          : null,
-                    ),
-                    child: Container(
-                      width: (MediaQuery.sizeOf(context).width / 2) - 35,
-                      height: 100,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.fill,
-                              scale: 2,
-                              image: AssetImage(Assets.imagesAnimalPhoto1))),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 5, left: 10),
-                            child: Text(
-                              "adoption",
-                              style: AppStyles.urbanistMedium16(context)
-                                  .copyWith(color: ColorsApp.primaryColor),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                )
+                SelectTypeOfRequest(
+                    type: "adoption",
+                    onTap: () {
+                      setState(() {
+                        selectedType = "adoption";
+                      });
+                    },
+                    isSelected: selectedType == "adoption",
+                    borderColor: ColorsApp.primaryColorOpicaty,
+                    image: Assets.imagesAnimalPhoto5,
+                    textColor: ColorsApp.primaryColor)
               ],
             )
           ],
---
+++ lib/view/adoption  and help request page/widgets/selectTypeOfRequest.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:graduation_project/core/util/styles.dart';
+
+class SelectTypeOfRequest extends StatefulWidget {
+  const SelectTypeOfRequest(
+      {super.key,
+      required this.type,
+      required this.onTap,
+      required this.isSelected,
+      required this.borderColor,
+      required this.image,
+      required this.textColor});
+
+  final String type;
+  final VoidCallback onTap;
+  final bool isSelected;
+  final Color borderColor;
+  final String image;
+  final Color textColor;
+  @override
+  State<SelectTypeOfRequest> createState() => _SelectTypeOfRequestState();
+}
+
+class _SelectTypeOfRequestState extends State<SelectTypeOfRequest> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+          border: widget.isSelected
+              ? Border.all(color: widget.borderColor, width: 3, strokeAlign: 1)
+              : null,
+        ),
+        child: Container(
+          width: (MediaQuery.sizeOf(context).width / 2) - 35,
+          height: 100,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              scale: 2,
+              image: AssetImage(widget.image),
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 5, left: 10),
+                child: Text(
+                  widget.type,
+                  style: AppStyles.urbanistMedium16(context)
+                      .copyWith(color: widget.textColor),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
