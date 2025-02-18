f7cb5908415b3a8f5d287634a3e33f7803ac02dc
Abdallah Reda <abdallahreda664@gmail.com>
2025-02-18 16:47:18 +0200

    adoption and Help Request handled

--- lib/view/signup completed page/signupCompletedPage.dart
+++ lib/view/signup completed page/signupCompletedPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:graduation_project/core/Widgets/customButton.dart';
 import 'package:graduation_project/core/util/appImages.dart';
 import 'package:graduation_project/core/util/styles.dart';
